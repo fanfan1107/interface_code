b0cf47ba35eb007be2d1d1d1e7e5aba87e468a14
“hhy” <1614561214@qq.com>
2021-06-25 14:22:04 +0800

    second commit

--- src/test/java/com/lemon/data/Constants.java
+++ src/test/java/com/lemon/data/Constants.java
@@ -6,9 +6,9 @@ public class Constants {
     //Excel文件的路径
     public static final String EXCEL_FILE_PATH=System.getProperty("user.dir")+"/src/test/resources/api_testcases_futureloan_v4.xls";
     //接口BaseUrl地址
-    public static final String BASE_URI="http://8.129.52.205:8012/futureloan";
+    public static final String BASE_URI="http://47.102.208.69:8899//futureloan";
     //数据库baseuri
-    public static final String DB_BASE_URI="8.129.52.205:33060";
+    public static final String DB_BASE_URI="47.102.208.69:3307";
     //数据库名
     public static final String DB_NAME="futureloan";
     public static final String DB_USERNAME="root";
